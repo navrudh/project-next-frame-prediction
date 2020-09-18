5980360320c1aa26a238bdab8ae2f0dd6e837428
Dhruvan Ganesh <navrudh@gmail.com>
2020-09-18 02:18:32 +0200

    pred: update lr scheduler

--- train_video_prediction.py
+++ train_video_prediction.py
@@ -70,7 +70,9 @@ class SelfSupervisedVideoPredictionLitModel(LightningModule):
         optimizer = torch.optim.Adam(
             self.model.parameters(), lr=self.lr, weight_decay=1e-5
         )
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.5)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, patience=3, factor=0.5
+        )
         return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_nb):
