454d1390233ec61d8e1f9862b883f55c260b7631
Dhruvan <dhruvan.ganesh@iais.fraunhofer.de>
2020-10-11 15:25:54 +0200

    utils: update download script

--- utils/download-dataset.sh
+++ utils/download-dataset.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
+UTILS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+DATASET_PATH=$(cat "$UTILS_DIR/../resources/config/user-$USER.json" | python -c "import json,sys;obj=json.load(sys.stdin);print(obj['ucf101']['path']);")
+
 if ! [ -x "$(command -v kaggle)" ]; then
   echo "kaggle-cli not found. Installing it."
   pip install kaggle
 fi
 
-kaggle datasets download -d pevogam/ucf101
 
-UTILS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DATASET_PATH=$(cat "$UTILS_DIR/../resources/config/user-$USER.json" | python -c "import json,sys;obj=json.load(sys.stdin);print(obj['ucf101']['path']);")
 
 FILE=ucf101.zip
-[[ -f "$FILE" ]] && echo "$FILE exists." || {echo "$FILE does not exist." && exit 1}
+
+if [[ -f "$FILE" ]]; then
+   echo "$FILE exists"
+else
+   kaggle datasets download -d pevogam/ucf101
+fi
 
 mkdir -p "$DATASET_PATH/"
 mv "$FILE" "$DATASET_PATH/"
-cd "$DATASET_PATH/"
+cd "$DATASET_PATH/" || exit 1
 7z x "$FILE"
