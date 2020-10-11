73fad8d6fd2bbb5ea3abf24fe2fd79a3e5c2c16e
Dhruvan Ganesh <navrudh@gmail.com>
2020-10-11 17:08:08 +0200

    utils: add cleaning scripts

---
+++ utils/clean-workdir.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+UTILS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+WORKDIR=$(cat "$UTILS_DIR/../resources/config/user-$USER.json" | python -c "import json,sys;obj=json.load(sys.stdin);print(obj['workdir']);")
+
+rm -rf "$WORKDIR"
